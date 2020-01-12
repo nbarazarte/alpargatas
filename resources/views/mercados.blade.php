0a7db80c881c60b96eb5633485d016b96045793a
Neil Barazarte <ezebarazarte@gmail.com>
2020-01-12 19:26:05 -0400

    mercados listo media querys fase beta

--- resources/views/mercados.blade.php
+++ resources/views/mercados.blade.php
@@ -4,21 +4,69 @@
 
 <style type="text/css">
 
-iframe{
+@media only screen and (max-width: 375px) {
 
-	width:100%;
-	height:1040px;
-	position: absolute;
-	top: -130px;
-}	
+	iframe{
 
-div .cryptowathframe{
+		width:100%;
+		height:1230px;
+		position: absolute;
+		top: -236px;
 
-	border: 1px solid black;
-	position: relative;
-	width: 100%;
-	height: 769px;
-	top: -141px;
+	}	
+
+	div .cryptowathframe{
+
+		border: 1px solid black;
+		position: relative;
+		width: 100%;
+		height: 880px;
+		top: -100px;
+	}
+
+} 
+
+@media only screen and (max-width: 768px) {
+
+	iframe{
+
+		width:100%;
+		height:1080px;
+		position: absolute;
+		top: -100px;
+
+	}	
+
+	div .cryptowathframe{
+
+		border: 1px solid black;
+		position: relative;
+		width: 100%;
+		height: 790px;
+		top: -141px;
+	}
+
+} 
+
+@media screen and (max-width: 1440px) { 
+
+	iframe{
+
+		width:100%;
+		height:1040px;
+		position: absolute;
+		top: -130px;
+	}	
+
+	div .cryptowathframe{
+
+		border: 1px solid black;
+		position: relative;
+		width: 100%;
+		height: 769px;
+		top: -141px;
+	}
+
 }
 
 </style>
